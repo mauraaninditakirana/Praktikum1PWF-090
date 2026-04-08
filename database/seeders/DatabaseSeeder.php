f7a25070d284bcb36808688c0552a9bc91d04144
mauraaninditakirana <kiranamaura351@gmail.com>
2026-04-08 15:10:49 +0700

    Menambahkan akun Admin dan User Biasa pada DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,12 +10,24 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-        
-        Product::factory(20)->create();
-        Kategori::factory(10)->create();
+    // 1. Buat akun Admin
+    User::factory()->create([
+        'name' => 'Maura Admin',
+        'email' => 'admin@gmail.com',
+        'password' => bcrypt('password'), // Password: password
+        'role' => 'admin', // Ini role yang baru kita tambahkan
+    ]);
+
+    // 2. Buat akun User Biasa
+    User::factory()->create([
+        'name' => 'Maura User',
+        'email' => 'user@gmail.com',
+        'password' => bcrypt('password'), // Password: password
+        'role' => 'user',
+    ]);
+
+    // 3. data dummy untuk Kategori & Product
+    Kategori::factory(10)->create();
+    Product::factory(20)->create();
     }
 }
